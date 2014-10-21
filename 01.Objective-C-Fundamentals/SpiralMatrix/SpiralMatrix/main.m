2dca264f40ff4415d25eafad7fae17431eb5176d
JR <admin@Mac-Admin.local>
2014-10-22 02:23:44 +0300

    spiral matrix

---
+++ 01.Objective-C-Fundamentals/SpiralMatrix/SpiralMatrix/main.m
@@ -0,0 +1,73 @@
+//
+//  main.m
+//  SpiralMatrix
+//
+//  Created by JR on 10/21/14.
+//  Copyright (c) 2014 JR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void printMatrix(NSArray *matrix) {
+    NSMutableString *buffer = [[NSMutableString alloc] init];
+    for (NSArray *row in matrix) {
+        for (NSNumber *cell in row) {
+            [buffer appendFormat:@"%3d", [cell intValue]];
+        }
+        [buffer appendString:@"\n"];
+    }
+    
+    NSLog(@"\n%@", buffer);
+}
+
+NSMutableArray* createSpiralMatrix(int n) {
+    
+    int dx = 1;
+    int dy = 0;
+    int x = 0;
+    int y = 0;
+    
+    // Initialize matrix
+    NSMutableArray *matrix = [[NSMutableArray alloc] initWithCapacity:n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = [[NSMutableArray alloc] init];
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = [NSNull null];
+        }
+    }
+    
+    // Fill matrix
+    for (int i = 0; i < n * n; i++) {
+        matrix[y][x] = [NSNumber numberWithInt:i+1];
+        int nx = x + dx;
+        int ny = y + dy;
+        if (0 <= nx && nx < n && 0 <= ny && ny < n && [matrix[ny][nx] isEqual:[NSNull null]]) {
+            x = nx;
+            y = ny;
+        } else {
+            int temp = dy;
+            dy = dx;
+            dx = -temp;
+            x = x + dx;
+            y = y + dy;
+        }
+    }
+    
+    return matrix;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+      
+        NSArray *spiral = @[
+          @[@1, @2, @3],
+          @[@8, @9, @4],
+          @[@7, @6, @5]
+        ];
+        printMatrix(spiral);
+        
+        NSMutableArray *matrix = createSpiralMatrix(4);
+        printMatrix(matrix);
+    }
+    return 0;
+}
