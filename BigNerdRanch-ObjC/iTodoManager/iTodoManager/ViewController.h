4b92450fb567eab9f9f74e4262ed01cbf58b29eb
JR <jrusev@gmail.com>
2014-10-24 01:53:33 +0300

    added todo app

---
+++ BigNerdRanch-ObjC/iTodoManager/iTodoManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTodoManager
+//
+//  Created by JR on 10/24/14.
+//  Copyright (c) 2014 JR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
