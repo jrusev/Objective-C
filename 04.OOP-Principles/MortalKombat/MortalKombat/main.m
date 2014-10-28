f97eae68b262d381a98370dc2ee04c1a98f17e69
JR <jrusev@gmail.com>
2014-10-29 01:28:03 +0200

    removed courses

--- 04.OOP-Principles/MortalKombat/MortalKombat/main.m
+++ 04.OOP-Principles/MortalKombat/MortalKombat/main.m
@@ -1,19 +1,17 @@
 //
 //  main.m
-//  VowelCounter
+//  MortalKombat
 //
-//  Created by JR on 10/25/14.
+//  Created by JR on 10/29/14.
 //  Copyright (c) 2014 JR. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "NSString+VowelCounting.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
-        NSString *str = @"Hello, World!";
-        NSLog(@"'%@' has %d vowels", str, [str vowelCount]);
+        // insert code here...
+        NSLog(@"Hello, World!");
     }
     return 0;
 }
